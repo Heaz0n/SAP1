2ae79a3ce82faaba4bbd200aeb1fd396a9154e97
Heaz0n <evengequiter179@yandex.ru>
2024-06-12 01:27:22 +0500

    Add info routes and controller methods

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,14 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\InfoController;
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+Route::prefix('info')->group(function () {
+    Route::get('server', [InfoController::class, 'serverInfo']);
+    Route::get('client', [InfoController::class, 'clientInfo']);
+    Route::get('database', [InfoController::class, 'databaseInfo']);
 });
