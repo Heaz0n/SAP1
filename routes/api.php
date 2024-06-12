7509f92ce24eb586f7905c1d821cec8e0379e487
Heaz0n <evengequiter179@yandex.ru>
2024-06-12 23:41:16 +0500

    обновил api

--- routes/api.php
+++ routes/api.php
@@ -15,25 +15,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::group(['prefix' => '/auth'], function () {
+Route::prefix('auth')->group(function () {
     Route::get('/home', function(){
         return view('welcome');
     });
-
-    Route::post('/login', [AuthController::class, "login"]);
-    Route::middleware('authcheck')->group(function() {
-
-        Route::get('/register', function(){
-            return view('welcome');
-        });
-
-        Route::post('/register', [AuthController::class, "register"]);
-    });
-
-    Route::group(['middleware' => 'auth:api'], function () {
-        Route::get('/me', [AuthController::class, "me"]);
-        Route::post('/out', [AuthController::class, "logout"]);
-        Route::get('/tokens', [AuthController::class, "tokens"]);
-        Route::post('/out_all', [AuthController::class, "logoutAll"]);
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::middleware('auth:api')->group(function () {
+        Route::get('me', [AuthController::class, 'me']);
+        Route::post('out', [AuthController::class, 'logout']);
+        Route::get('tokens', [AuthController::class, 'tokens']);
+        Route::post('out_all', [AuthController::class, 'logoutAll']);
     });
 });
